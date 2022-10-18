dafb42171e39b87b63c0c12afa0a47bd0dbbe47d
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-10-18 19:25:36 -0400

    mongoDB connection

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const indexController = require('./controllers/index_controller')
+const indexController = require("./controllers/index_controller");
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //routes
-app.use('/', indexController);
+app.use("/recipe", indexController);
 
 //db connection
 mongoose
   .connect(process.env.MONGO_URI, {
-
-    //got an error with this guy 
-
-    //userNewUrlParser: true, 
-
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB connected!!"))
+  .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
