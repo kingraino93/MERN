b3d09491dd704021f1e649193f3ed5b381591d99
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-10-18 19:02:23 -0400

    recipe model

---
+++ models/recipe.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//schema fields
+const recipeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: String,
+      required: true,
+    },
+    cuisine: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+      enum: ["easy", "medium", "hard"],
+    },
+    rating: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3, 4, 5],
+    },
+    serving: {
+      type: Number,
+      required: true,
+    },
+    prepTime: {
+      type: Number,
+      required: true,
+    },
+    cookTime: {
+      type: Number,
+      required: true,
+    },
+    steps: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//EXPORT
+
+module.exports = mongoose.model("Recipe", recipeSchema);
