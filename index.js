41ad1075201aca1d8d1fb5822e659048cc6005d1
Tom Christian <thomaspchristian@outlook.com>
2022-10-18 20:41:35 -0400

    heroku fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/recipe", indexController);
 
 //NOT FOUND
-index.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.status(404)
   res.send('Page Not Found')
 })
@@ -27,6 +27,6 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
